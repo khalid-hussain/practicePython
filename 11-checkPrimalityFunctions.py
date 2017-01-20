bddf893848386426ded2f914d07443c258eb9e47
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-20 22:13:04 +0800

    Improved solution to problem 11

--- 11-checkPrimalityFunctions.py
+++ 11-checkPrimalityFunctions.py
@@ -1,10 +1,20 @@
 def isPrime(n):
-    divisors = [x for x in range(1, n + 1) if n % x == 0]
-    noOfDivisors = len(divisors)
-    if noOfDivisors > 2:
-        return False
-    else:
-        return True
+    counter = 2
+    while counter != n:
+        if n % counter == 0:
+            return False
+        counter += 1
+    return True
+
+    # divisors = [x for x in range(1, n + 1) if n % x == 0]
+    # noOfDivisors = len(divisors)
+    # if noOfDivisors > 2:
+    #     return False
+    # else:
+    #     return True
 
 
 print(isPrime(4))
+print(isPrime(99))
+print(isPrime(29))
+print(isPrime(31))
