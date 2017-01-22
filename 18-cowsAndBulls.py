579af14d1c907e848d77ce9d5e1b5400481ad637
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-22 15:02:24 +0800

    Attempt at problem 18

---
+++ 18-cowsAndBulls.py
@@ -0,0 +1,10 @@
+import random
+import math
+
+# numberToGuess = str(random.randint(1000, 9999))
+numberToGuess = 2453
+numberToGuess = str(numberToGuess)
+print(numberToGuess)
+
+guessedNumber = int(input("Enter your guess: "))
+guessedNumber = str(guessedNumber)
