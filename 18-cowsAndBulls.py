d67f948c67f165f6c69a233da5882ab09354e1e5
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-23 15:04:02 +0800

    Updated solution to problem 18

--- 18-cowsAndBulls.py
+++ 18-cowsAndBulls.py
@@ -1,10 +1,32 @@
 import random
-import math
 
-# numberToGuess = str(random.randint(1000, 9999))
-numberToGuess = 2453
-numberToGuess = str(numberToGuess)
-print(numberToGuess)
 
-guessedNumber = int(input("Enter your guess: "))
-guessedNumber = str(guessedNumber)
+def checkCowsAndBulls(numberToGuess: int, guessedNumber: int):
+    strGuessedNumber = str(guessedNumber)
+    strNumberToGuess = str(numberToGuess)
+
+    cows = 0
+    bulls = 0
+
+    for k in range(0, 4):
+        if strGuessedNumber[k] == strNumberToGuess[k]:
+            cows += 1
+        elif strGuessedNumber[k] in strNumberToGuess:
+            bulls += 1
+
+    return [cows, bulls]
+
+
+if __name__ == "__main__":
+    print("Welcome to the Cows and Bulls Game")
+    numberToGuess = random.randint(1000, 9999)
+    numberToGuess = 4018
+    print("NumbertoGuess:", numberToGuess)
+
+    while True:
+        guessedNumber = input("Enter your guesses. To Exit, type 'q': ")
+        if guessedNumber == 'q':
+            break
+        else:
+            result = checkCowsAndBulls(numberToGuess, guessedNumber)
+            print("Cows:", result[0], "Bulls:", result[1])
