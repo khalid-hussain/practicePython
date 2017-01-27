575cc2e7b185309347921bf0bab7f7d6d40f63be
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-27 09:50:32 +0800

    Improved solution

--- 21-writeToFile.py
+++ 21-writeToFile.py
@@ -11,14 +11,11 @@ base_url = "http://www.nytimes.com"
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-open_file = open("21-writeToFileOutput.txt", "w")
-
-for story_heading in soup.find_all(class_="story-heading"):
-    if story_heading.a:
-        open_file.write(story_heading.a.text.replace("\n", " ").strip())
-        # print(story_heading.a.text.replace("\n", " ").strip())
-    else:
-        open_file.write(story_heading.contents[0].strip())
-        # print(story_heading.contents[0].strip())
-
-open_file.close()
+with open('21-writeToFileOutput.txt', 'w') as open_file:
+    for story_heading in soup.find_all(class_="story-heading"):
+        if story_heading.a:
+            open_file.write(story_heading.a.text.replace("\n", " ").strip())
+            # print(story_heading.a.text.replace("\n", " ").strip())
+        else:
+            open_file.write(story_heading.contents[0].strip())
+            # print(story_heading.contents[0].strip())
